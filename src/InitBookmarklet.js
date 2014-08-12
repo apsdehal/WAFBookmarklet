bca30f047812441a781799f19596f2fa622c7ebb
Amanpreet Singh <apsdehal@gmail.com>
2014-08-12 18:40:28 +0530

    Modifies again

--- src/InitBookmarklet.js
+++ src/InitBookmarklet.js
@@ -23,7 +23,7 @@
         l = document.createElement('link');
 
     l.rel = 'stylesheet';
-    l.href = 'http://raw.githubusercontent.com/apsdehal/WAFBookmarklet/master/css/pundit.css';
+    l.href = 'https://rawgithub.com/apsdehal/WAFBookmarklet/master/css/pundit.css';
     l.type = 'text/css';
     l.media = 'screen';
     l.charset = 'utf-8';
@@ -47,11 +47,11 @@
     djConfig = {
         afterOnLoad: true,
         useXDomain: true,
-        baseUrl: "http://raw.githubusercontent.com/apsdehal/WAFBookmarklet/master/dojo/",
+        baseUrl: "https://rawgithub.com/apsdehal/WAFBookmarklet/master/dojo/",
         require: ["dojo.Bookmarklet"]
     };
     d.type = 'text/javascript';
-    d.src = 'http://raw.githubusercontent.com/apsdehal/WAFBookmarklet/master/dojo/dojo.xd.js';
+    d.src = 'https://rawgithub.com/apsdehal/WAFBookmarklet/master/dojo/dojo.xd.js';
     h.appendChild(d);
 
 })();
